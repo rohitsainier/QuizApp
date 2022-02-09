60708efbe8443a45711c971ed3289436898c5089
rohitsainier <rohitsainier@gmail.com>
2022-02-10 01:21:06 +0530

    Refactored the questionviewcontroller unit tests

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -16,10 +16,13 @@ class QuestionViewController: UIViewController{
     //MARK: - VARIABLES
     private var question: String = ""
     private var options: [String] = []
-    convenience init(question: String,options: [String]){
+    private var selection: ((String) -> Void)? = nil
+    private let reuseIdentifier: String = "Cell"
+    convenience init(question: String,options: [String], selection: @escaping (String) -> Void){
         self.init()
         self.question = question
         self.options = options
+        self.selection = selection
     }
     
     override func viewDidLoad() {
@@ -28,13 +31,24 @@ class QuestionViewController: UIViewController{
     }
 }
 
-extension QuestionViewController: UITableViewDataSource{
+extension QuestionViewController: UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return options.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = dequeueCell(in: tableView)
         cell.textLabel?.text = options[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection?(options[indexPath.row])
+    }
+    
+    private func dequeueCell(in tableView: UITableView) -> UITableViewCell{
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier){
+            return cell
+        }
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
     }
 }
--- QuizAppTests/QuestionViewControllerTest.swift
+++ QuizAppTests/QuestionViewControllerTest.swift
@@ -12,28 +12,46 @@ import XCTest
 class QuestionViewControllerTest: XCTestCase{
     
     func test_viewDidLoad_QuestionHeaderText(){
-        let sut = QuestionViewController(question: "Q1",options: [])
-        _ = sut.view
+        let sut = makeSUT(question: "Q1")
         XCTAssertEqual(sut.headerLabel.text, "Q1")
     }
     
-    func test_viewDidLoad_withNoOptions_renderZeroOptions(){
-        let sut = QuestionViewController(question: "Q1",options: [])
-        _ = sut.view
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    func test_viewDidLoad_renderOptions(){
+        XCTAssertEqual(makeSUT(options:[]).tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(options:["A1"]).tableView.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(makeSUT(options:["A1","A2"]).tableView.numberOfRows(inSection: 0), 2)
     }
     
-    func test_viewDidLoad_withOneOption_renderOneOption(){
-        let sut = QuestionViewController(question: "Q1",options: ["A1"])
-        _ = sut.view
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    func test_viewDidLoad_renderOptionText(){
+        XCTAssertEqual(makeSUT(options: ["A1","A2"]).tableView.title(at: 0), "A1")
+        XCTAssertEqual(makeSUT(options: ["A1","A2"]).tableView.title(at: 1), "A2")
     }
     
-    func test_viewDidLoad_withOneOption_renderOneOptionText(){
-        let sut = QuestionViewController(question: "Q1",options: ["A1"])
+    func test_optionSelected_notifyDelegate(){
+        var receiveAnswer = ""
+        let sut = makeSUT(options: ["A1"]){
+            receiveAnswer = $0
+        }
+        let indexpath = IndexPath(row: 0, section: 0)
+        sut.tableView.delegate?.tableView?(sut.tableView, didSelectRowAt: indexpath)
+        XCTAssertEqual(receiveAnswer, "A1")
+    }
+    
+    //MARK: - Helpers
+    
+    func makeSUT(question: String = "", options: [String] = [],selection: @escaping (String) -> Void = {_ in }) -> QuestionViewController{
+        let sut = QuestionViewController(question:   question,options: options,selection: selection)
         _ = sut.view
-        let indexPath = IndexPath(row: 0, section: 0)
-        let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
-        XCTAssertEqual(cell?.textLabel?.text, "A1")
+        return sut
+    }
+}
+
+
+private extension UITableView{
+    func cell(at row: Int) -> UITableViewCell?{
+        return dataSource?.tableView(self, cellForRowAt: IndexPath(row: row, section: 0))
+    }
+    func title(at row: Int) -> String?{
+        return cell(at: row)?.textLabel?.text
     }
 }
